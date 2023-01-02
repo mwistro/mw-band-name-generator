f0e252e4c6d118a46531b545e48c90c9a68c368c
mwistro <40882515+mwistro@users.noreply.github.com>
2023-01-02 21:35:59 +0100

    Add files via upload

---
+++ mw-name-band-gen.py
@@ -0,0 +1,6 @@
+print("WELCOME TO THE BAND NAME GENERATOR")
+scary = input("Type a random scary adjective:\n")
+place_something = input("A place or something that has special significance to your band:\n")
+body_part = input("A part of the body :\n")
+
+print("Your band name could be something like: " + scary + " " + place_something + " " + body_part)
